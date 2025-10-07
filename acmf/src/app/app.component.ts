7e9765400888974b559094f88dadf51c926b7a73
Jonathan Mulinge <jonathanmulinge@gmail.com>
2025-10-07 12:30:17 -0700

    chore: update app component logic and improve initialization structure

--- acmf/src/app/app.component.ts
+++ acmf/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { SidebarComponent } from "./components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
