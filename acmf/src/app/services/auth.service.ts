903c648aaaa20c1586e3d56229b861d61da0a9b5
Jonathan Mulinge <jonathanmulinge@gmail.com>
2025-11-07 01:14:51 -0800

    changed local urls

--- acmf/src/app/services/auth.service.ts
+++ acmf/src/app/services/auth.service.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 import { ProfileService } from './profile.service';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 
 @Injectable({
