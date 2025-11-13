c2bfe1bdc5a98fcd81a5c483fea5ad6f53ad4247
Jonathan Mulinge <jonathanmulinge@gmail.com>
2025-11-13 01:31:54 -0800

    Add modern loading state to search and filter inputs with TailwindCSS skeletons

---
+++ acmf/src/app/groups/grou-formation/grou-formation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grou-formation',
+  imports: [],
+  templateUrl: './grou-formation.component.html',
+  styleUrl: './grou-formation.component.css'
+})
+export class GrouFormationComponent {
+
+}
