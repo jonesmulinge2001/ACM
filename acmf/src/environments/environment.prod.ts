7fbc7509a57102e6644c6af428fb12f2febbfe11
Jonathan Mulinge <jonathanmulinge@gmail.com>
2026-01-15 21:18:38 -0800

    chore: update production environment configuration

--- acmf/src/environments/environment.prod.ts
+++ acmf/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBase: 'https://acm-1.onrender.com',
-  socketBase: 'https://acm-1.onrender.com/groups',
-  socketBase1: 'https://acm-1.onrender.com/conversations',
+  apiBase: 'https://acm-1-6bt0.onrender.com/',
+  socketBase: 'https://acm-1-6bt0.onrender.com/groups',
+  socketBase1: 'https://acm-1-6bt0.onrender.com/conversations',
 };
