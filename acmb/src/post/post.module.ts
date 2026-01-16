b085ec9e66dce8602ede8b0ab413662e4707dd77
Jonathan Mulinge <jonathanmulinge@gmail.com>
2026-01-15 20:52:56 -0800

    fix: remove src alias from post module import paths

--- acmb/src/post/post.module.ts
+++ acmb/src/post/post.module.ts
@@ -2,7 +2,7 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
-import { CloudinaryModule } from 'src/shared/cloudinary/cloudinary/cloudinary.module';
+import { CloudinaryModule } from '../shared/cloudinary/cloudinary/cloudinary.module';
 
 @Module({
   imports: [CloudinaryModule],
